2227cf3cbb798918c9e7d2e9965b117d0d0595f4
quietinvestor <quietinvestor@localhost.localdomain>
2020-06-16 19:04:57 +0200

    Adding data_structures.py.

---
+++ data_stuctures.py
@@ -0,0 +1,17 @@
+proposal = {
+        'proposal_number': '',
+        'proposal': '',
+        'management_recommendation': '',
+        'vote_cast': '',
+        'sponsor': ''
+        }
+
+shareholder_meeting = {
+        'company_name': '',
+        'ticker': '',
+        'security_id': '',
+        'meeting_type': '',
+        'meeting_date': '',
+        'record_date': '',
+        'voting_record': [] # List of proposals.
+        }
